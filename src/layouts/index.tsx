bde84ed6915bb8d4148d4204d3ee3d34ad16ceb0
redblue123 <wohaishiwonihaome@gmail.com>
2023-12-26 09:42:06 +0800

    修改了logo

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,6 +2,8 @@ import { ProLayout } from '@ant-design/pro-layout';
 // import { ProLayout } from '@ant-design/pro-components';
 import { Link, Outlet, useAppData, useLocation } from 'umi';
 // import { SmileOutlined } from '@ant-design/icons';
+import SmileUrl, { ReactComponent as SvgSmile } from '../../public/logo.svg';//使用import语句导入logo.svg文件
+// import imgUrl from '~/public/logo.svg'
  
 
 export default function Layout() {
@@ -12,9 +14,11 @@ export default function Layout() {
     
     <ProLayout 
       route={clientRoutes[0]}
-      location={location}
+      location={location}      
       title="Portal Design"
-      // logo="./static/images/logo.svg"
+      
+      logo={SmileUrl}
+      // logo={<img src="https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00327-3368.jpg" alt="Logo" />} 
       menuItemRender={(menuItemProps, defaultDom) => {
         if (menuItemProps.isUrl || menuItemProps.children) {
           return defaultDom;
